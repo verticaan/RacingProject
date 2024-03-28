75f99c993ef783ab962e75342818bb4093d8d194
Odafe Jagboro <63778072+verticaan@users.noreply.github.com>
2024-03-28 19:41:31 +0000

    tooltip

---
+++ Assets/ToolTip.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode()]
+public class ToolTip : MonoBehaviour
+{
+    public Text headerField;
+
+    public Text contentField;
+
+    public LayoutElement layoutElement;
+
+    public int characterWrapLimit;
+
+    public void SetText(string content, string header = "")
+    {
+        if (string.IsNullOrEmpty(header))
+        {
+            headerField.gameObject.SetActive(false);
+        }
+        else
+        {
+            headerField.gameObject.SetActive(true);
+            headerField.text = header;
+        }
+
+        contentField.text = content;
+
+        int headerLength = headerField.text.Length;
+        int contentLength = contentField.text.Length;
+
+        layoutElement.enabled = (headerLength > characterWrapLimit || contentLength > characterWrapLimit) ? true : false;
+    }
+
+}
